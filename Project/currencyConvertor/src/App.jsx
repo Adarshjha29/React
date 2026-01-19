051c4680d1de17b70c8b033e21d05ed9c4061903
adarsh jha <adarshjha430@gmail.com>
2026-01-19 13:09:28 +0530

    Updated

--- Project/currencyConvertor/src/App.jsx
+++ Project/currencyConvertor/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
                     <div className="relative w-full h-0.5">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 cursor-pointer"
                             onClick={swap}
                         >
                             swap
@@ -70,7 +70,7 @@ function App() {
                             amountDisable
                         />
                     </div>
-                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg cursor-pointer">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
